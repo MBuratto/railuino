6da37657ff0292365f033ecb0b56b7e68dd4f528
joerg.pleumann@gmail.com <joerg.pleumann@gmail.com@0198248e-a4d5-1c42-32e5-e8c25e623dac>
2012-12-08 10:53:00 +0000

    Tagging version 0.5

---
+++ tags/Railuino-0.5/src/examples/01.Controller/Power/Power.ino
@@ -0,0 +1,40 @@
+/*********************************************************************
+ * Railuino - Hacking your Märklin
+ *
+ * Copyright (C) 2012 Joerg Pleumann
+ * 
+ * This example is free software; you can redistribute it and/or
+ * modify it under the terms of the Creative Commons Zero License
+ * as published by the Creative Commons Organisation, version 1.0.
+ * This effectively puts the file into the public domain.
+ *
+ * This example is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LICENSE file for more details.
+ */
+ 
+#include <Railuino.h>
+
+const word    TIME  = 2000;
+const boolean DEBUG = true;
+
+TrackController ctrl(0xdf24, DEBUG);
+
+void setup() {
+  Serial.begin(115200);
+  ctrl.begin();
+}
+
+void loop() {
+  Serial.println("Power on");
+  ctrl.setPower(true);
+  
+  delay(TIME);
+  
+  Serial.println("Power off");
+  ctrl.setPower(false);
+  
+  delay(TIME);
+}
+
