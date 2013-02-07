e5ae2d222ecb8e1597dd65a92829314757f1a639
joerg.pleumann@gmail.com <joerg.pleumann@gmail.com@0198248e-a4d5-1c42-32e5-e8c25e623dac>
2013-02-07 19:06:53 +0000

    Corrected license header for test suite

--- trunk/Railuino/src/examples/05.Misc/Tests/Tests.ino
+++ trunk/Railuino/src/examples/05.Misc/Tests/Tests.ino
@@ -3,10 +3,10 @@
  *
  * Copyright (C) 2012 Joerg Pleumann
  * 
- * This example is free software; you can redistribute it and/or
- * modify it under the terms of the Creative Commons Zero License,
- * version 1.0, as published by the Creative Commons Organisation.
- * This effectively puts the file into the public domain.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
